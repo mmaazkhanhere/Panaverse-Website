ef6f40a1b429a22bb2c9c8aaeba2de2d2ce0dbb0
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-03-02 13:02:24 +0500

    Home Page Complete

--- panaverse-website/src/app/Component/Banner.tsx
+++ panaverse-website/src/app/Component/Banner.tsx
@@ -10,18 +10,20 @@ export default function Banner() {
 
     return (
         <>
-            <Box bgColor=' rgba(255, 244, 223, 1)' borderRadius='30px' mr={{ lg: '100', base: '50' }} ml={{ base: '50', lg: '100' }}>
+
+            <Box bgColor=' rgba(255, 244, 223, 1)' borderRadius='30px' mr={{ lg: '100px', base: '50px' }} ml={{ base: '50px', lg: '100' }}>
+                {/*Banner Box */}
                 <Center>
                     <Container maxW='1800px'>
-                        <SimpleGrid templateColumns={{ lg: 'repeat(2,1fr)', base: 'repeat(1,1fr)' }}>
+                        <SimpleGrid SimpleGrid templateColumns={{ lg: 'repeat(2,1fr)', base: 'repeat(1,1fr)' }}>
 
-                            <Box flexBasis='50%' ml='-20px'>
+                            <Box flexBasis='50%' ml={{ base: '0px', lg: '-20px' }} >
                                 <Image src='/metaverse-exp.png' alt='Metaverse Experience' borderRadius='30px' w={{ base: '293', lg: '650' }} h={{ base: '160', lg: '370' }} />
                             </Box>
 
-                            <Box flexBasis='50%' placeItems='center' textAlign='center' lineHeight={{ base: '17px', lg: '40px' }} mt={{ base: '5px', lg: '90px' }}>
+                            <Box flexBasis='50%' placeItems='center' textAlign='center' lineHeight={{ base: '17px', lg: '40px' }} mt={{ base: '5px', lg: '100px' }}>
 
-                                <Heading fontSize={{ lg: '38px', base: '12px' }} fontWeight='bold'>CERTIFIED WEB 3.0 AND METAVERSE DEVELOPER</Heading>
+                                <Heading fontSize={{ lg: '40px', base: '20px' }} fontWeight='bold'>CERTIFIED WEB 3.0 AND METAVERSE DEVELOPER</Heading>
 
                                 <Text fontSize={{ lg: '20px', base: '12px' }}>
                                     A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar
@@ -32,20 +34,23 @@ export default function Banner() {
                 </Center>
             </Box >
 
-            <Box bgColor='rgba(255, 251, 244, 1)' pt={{ base: '10px', lg: '50px' }} ml={{ lg: '100px', base: '50px' }} mr={{ lg: '100px', base: '50px' }}>
+            <Box bgColor='rgba(255, 251, 244, 1)' pt={{ base: '20px', lg: '50px' }} ml={{ lg: '100px', base: '15px' }} mr={{ lg: '100px', base: '15px' }}>
+                {/*Courses Box */}
                 <Container maxW='1800px'>
-                    <SimpleGrid templateColumns='repeat(2,1fr)'>
+                    <SimpleGrid templateRows={{ base: 'repeat(2,1fr)', lg: 'none' }} templateColumns={{ lg: 'repeat(2,1fr)', base: 'none' }} gap={{ lg: '50px', base: '0px' }}>
 
-                        <Box mt={{ base: '5px', lg: '50px' }}>
-                            <Heading fontSize={{ lg: '38px', base: '12px' }} fontWeight='bold' textAlign='center'>SPECIALISATION WE OFFER</Heading>
+                        <Box mt={{ base: '20px', lg: '50px' }}>
+                            {/*Course Main Heading Box */}
+                            <Heading fontSize={{ lg: '40px', base: '20px' }} fontWeight='bold' textAlign='center'>SPECIALISATION WE OFFER</Heading>
 
-                            <Text fontSize={{ lg: '20px', base: '12px' }} textAlign='center' ml={{ lg: '100px', base: '50px' }} mr={{ lg: '100px', base: '50px' }}>
+                            <Text fontSize={{ lg: '20px', base: '12px' }} textAlign='center' >
                                 A 15-month long hybrid programme that includes both online and onsite classes, divided into five quarters, spanning 13 weeks each.
                             </Text>
 
                             <Link href={url} passHref>
                                 <Center>
-                                    <Button mt={{ lg: '20px', base: '10px' }} as="a" target='_blank' rel='noopener noreferrer' bgColor='black' textColor='white' textAlign='center' borderRadius='20px' >
+                                    <Button as="a" target='_blank' bgColor='black' textColor='white' textAlign='center' mt={{ lg: '20px', base: '10px' }}
+                                        borderRadius='20px' size={{ lg: 'lg', base: 'xs' }}>
                                         Apply Now
                                     </Button>
                                 </Center>
@@ -54,13 +59,14 @@ export default function Banner() {
                         </Box>
 
                         <Box>
+                            {/*Course with Images Boxes */}
                             <Container maxW={1800}>
-                                <SimpleGrid templateColumns='repeat(3,1fr)' gap={{ lg: '50px', base: '34px' }}>
+                                <SimpleGrid templateColumns='repeat(3,1fr)' gap={{ lg: '30px', base: '5px' }}>
                                     {courses.map((elem) => (
                                         <Box key={elem.id}>
                                             <Box>
                                                 <Image src={elem.src} alt={elem.alt} width={{ lg: '150px', base: '70px' }} height={{ lg: '89px', base: '41px' }} />
-                                                <Text textAlign='center' fontSize={{ lg: '18px', base: '10px' }}>{elem.text}</Text>
+                                                <Text fontSize={{ lg: '18px', base: '10px' }}>{elem.text}</Text>
                                             </Box>
 
                                         </Box>
