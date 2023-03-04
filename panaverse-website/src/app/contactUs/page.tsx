4a226aa00f58a6d895ffa21d6a0fa48de3232c50
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-03-04 15:04:54 +0500

    aboutUs page created and completed

---
+++ panaverse-website/src/app/contactUs/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import { Box, Container, SimpleGrid, Heading, Text, Image, Center, Button } from '@chakra-ui/react'
+import Link from 'next/link'
+
+export default function contactUs() {
+    return (
+        <Box>
+
+        </Box>
+    )
+}
