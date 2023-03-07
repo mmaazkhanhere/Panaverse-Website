42cd29a43c7c678afea5d334f710a4aba6aba061
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-03-07 16:28:50 +0500

    genomics and network syllabus animation added

--- panaverse-website/src/app/Genomics/Quarter2.tsx
+++ panaverse-website/src/app/Genomics/Quarter2.tsx
@@ -5,6 +5,7 @@ import { Box, Center, Container, Heading, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import Image from 'next/image'
 import info from '../../../public/bioInfo.jpg'
+import { RevealWrapper } from 'next-reveal'
 
 export default function Quarter2() {
 
@@ -13,42 +14,69 @@ export default function Quarter2() {
     }
 
     return (
-        <Box mt={{ base: '20px', lg: '50px' }} mb={{ base: '20px', lg: '50px' }} bgColor='rgba(245, 249, 255, 1)' borderRadius={{ base: '20px', lg: '30px' }}
+        <Box mt={{ base: '20px', lg: '50px' }} mb={{ base: '20px', lg: '50px' }} bgColor='rgba(189, 170, 171, 0.5)' borderRadius={{ base: '20px', lg: '30px' }}
             mr={{ lg: '100px', base: '30px' }} ml={{ base: '30px', lg: '100' }} pb={{ base: '20px', lg: '50px' }}>
             <Container maxW={1800}>
 
-                {/*Course Main Headlines */}
+                {/*Quarter Heading */}
 
-                <Heading pt='20px' textAlign='center' size={{ base: 'lg', lg: 'xl' }}>
-                    Quarter V
-                </Heading>
-                <Heading pt='20px' textAlign='center' size={{ base: 'lg', lg: 'xl' }}>
-                    Bio-361: Bioinformatics with Python
-                </Heading>
-                <Heading pt='10px' textAlign='center' size={{ base: 'md', lg: 'lg' }} fontWeight=''>Duration: 13 Weeks</Heading>
+                <RevealWrapper origin='right' delay={300} duration={1100} distance='500px' reset={true}>
+                    <Box mt='15px' mr={{ lg: '580px', base: '30px' }} ml={{ base: '30px', lg: '580px' }}>
+                        <Center>
+                            <Heading bgColor='white' textAlign='center' size={{ base: 'lg', lg: 'xl' }} borderRadius='30px' >
+                                Quarter V
+                            </Heading>
+                        </Center>
+                    </Box>
+                </RevealWrapper>
+
+                {/*Course Heading */}
+
+                <RevealWrapper origin='right' delay={300} duration={1200} distance='500px' reset={true}>
+                    <Box mt='15px' bgColor='white' borderRadius='30px' mr={{ lg: '300px', base: '30px' }} ml={{ base: '30px', lg: '300px' }}>
+                        <Heading textAlign='center' size={{ base: 'lg', lg: 'xl' }}>
+                            Bio-361: Bioinformatics with Python
+                        </Heading>
+                    </Box>
+                </RevealWrapper>
+
+                {/*Week Duration */}
+
+                <RevealWrapper origin='right' delay={300} duration={1300} distance='500px' reset={true}>
+                    <Box mt='15px' bgColor='white' borderRadius='30px' mr={{ lg: '420px', base: '30px' }} ml={{ base: '30px', lg: '420px' }}>
+                        <Heading textAlign='center' size={{ base: 'md', lg: 'lg' }} fontWeight=''>Duration: 13 Weeks</Heading>
+                    </Box>
+                </RevealWrapper>
 
                 {/*Image */}
-                <Center pt='20px' borderRadius={{ lg: '30', base: '20px' }}>
-                    <Image style={imageStyles} src={info} alt='Bioinformatics' />
-                </Center>
+                <RevealWrapper origin='right' delay={300} duration={1400} distance='500px' reset={true}>
+                    <Center pt='20px' borderRadius={{ lg: '30', base: '20px' }}>
+                        <Image style={imageStyles} src={info} alt='Bioinformatics' />
+                    </Center>
+                </RevealWrapper>
 
                 {/*Course Description} */}
 
-                <Heading pt='20px' as='u' size={{ base: 'sm', lg: 'lg' }}>Course Description</Heading>
-
-                <Text pt='20px' textAlign='justify' fontSize={{ lg: '24px', base: '16px' }}>
-                    In this course we will discover modern, next-generation sequencing libraries from the powerful Python ecosystem to perform cutting-edge research and analyze large
-                    amounts of biological data.
-                </Text>
+                <RevealWrapper origin='right' delay={300} duration={1500} distance='500px' reset={true}>
+                    <Box mt='15px' bgColor='white' borderRadius='30px' mr={{ lg: '830px', base: '140px' }}>
+                        <Heading as='u' size={{ base: 'sm', lg: 'lg' }}>Course Description</Heading>
+                    </Box>
+                    <Text pt='20px' textAlign='justify' fontSize={{ lg: '24px', base: '16px' }}>
+                        In this course we will discover modern, next-generation sequencing libraries from the powerful Python ecosystem to perform cutting-edge research and analyze large
+                        amounts of biological data.
+                    </Text>
+                </RevealWrapper>
 
                 {/*1st Task*/}
 
-                <Heading pt='10px' size={{ base: 'xs', lg: 'md' }}>Textbook</Heading>
-                <Link href='https://www.amazon.com/Bioinformatics-Python-Cookbook-applications-computational/dp/1803236426/ref=sr_1_1 ' target="_blank" rel="noopener noreferrer">
-                    <Text as='u' fontSize={{ lg: '20px', base: '14px' }} color='blue'>
-                        https://www.amazon.com/Bioinformatics-Python-Cookbook-applications-computational/dp/1803236426/ref=sr_1_1
-                    </Text>
-                </Link>
+                <RevealWrapper origin='right' delay={300} duration={1600} distance='500px' reset={true}>
+                    <Heading pt='10px' size={{ base: 'xs', lg: 'md' }}>Textbook</Heading>
+                    <Link href='https://www.amazon.com/Bioinformatics-Python-Cookbook-applications-computational/dp/1803236426/ref=sr_1_1 ' target="_blank" rel="noopener noreferrer">
+                        <Text as='u' fontSize={{ lg: '20px', base: '14px' }} color='blue'>
+                            https://www.amazon.com/Bioinformatics-Python-Cookbook-applications-computational/dp/1803236426/ref=sr_1_1
+                        </Text>
+                    </Link>
+                </RevealWrapper>
             </Container>
         </Box >
     )
--- panaverse-website/src/app/Network/Quarter1.tsx
+++ panaverse-website/src/app/Network/Quarter1.tsx
@@ -5,6 +5,7 @@ import { Box, Center, Container, Heading, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import Image from 'next/image'
 import CCNA from '../../../public/CCNA.png'
+import { RevealWrapper } from 'next-reveal'
 
 export default function Quarter1() {
 
@@ -13,43 +14,78 @@ export default function Quarter1() {
     }
 
     return (
-        <Box mt={{ base: '20px', lg: '50px' }} mb={{ base: '20px', lg: '50px' }} bgColor='rgba(245, 249, 255, 1)' borderRadius={{ base: '20px', lg: '30px' }}
+        <Box mt={{ base: '20px', lg: '50px' }} mb={{ base: '20px', lg: '50px' }} bgColor='rgba(189, 170, 171, 0.5)' borderRadius={{ base: '20px', lg: '30px' }}
             mr={{ lg: '100px', base: '30px' }} ml={{ base: '30px', lg: '100' }} pb={{ base: '20px', lg: '50px' }}>
             <Container maxW={1800}>
 
                 {/*Course Main Headlines */}
 
-                <Heading pt='10px' textAlign='center' size={{ base: '2xl', lg: '3xl' }} fontWeight='bold'>Network Programmability and Automation Specialisation </Heading>
-                <Heading pt='20px' textAlign='center' size={{ base: 'lg', lg: 'xl' }}>
-                    Quarter IV
-                </Heading>
-                <Heading pt='20px' textAlign='center' size={{ base: 'lg', lg: 'xl' }}>
-                    NPA-351: CCNA 200-301 Certification
-                </Heading>
-                <Heading pt='10px' textAlign='center' size={{ base: 'md', lg: 'lg' }} fontWeight=''>Duration: 13 Weeks</Heading>
+                <RevealWrapper origin='left' delay={300} duration={1000} distance='500px' reset={true}>
+                    <Box bgColor='white' borderRadius='30px' mr={{ lg: '100px', base: '30px' }} ml={{ base: '30px', lg: '100' }}>
+                        <Center>
+                            <Heading textAlign='center' size={{ base: '2xl', lg: '3xl' }} fontWeight='bold'>Network Programmability and Automation Specialisation</Heading>
+                        </Center>
+                    </Box>
+                </RevealWrapper>
+
+                {/*Quarter Heading */}
+
+                <RevealWrapper origin='right' delay={300} duration={1100} distance='500px' reset={true}>
+                    <Box mt='15px' mr={{ lg: '580px', base: '30px' }} ml={{ base: '30px', lg: '580px' }}>
+                        <Center>
+                            <Heading bgColor='white' textAlign='center' size={{ base: 'lg', lg: 'xl' }} borderRadius='30px' >
+                                Quarter IV
+                            </Heading>
+                        </Center>
+                    </Box>
+                </RevealWrapper>
+
+                {/*Course Heading */}
+
+                <RevealWrapper origin='right' delay={300} duration={1200} distance='500px' reset={true}>
+                    <Box mt='15px' bgColor='white' borderRadius='30px' mr={{ lg: '300px', base: '30px' }} ml={{ base: '30px', lg: '300px' }}>
+                        <Heading textAlign='center' size={{ base: 'lg', lg: 'xl' }}>
+                            NPA-351: CCNA 200-301 Certification
+                        </Heading>
+                    </Box>
+                </RevealWrapper>
+
+                {/*Week Duration */}
+
+                <RevealWrapper origin='right' delay={300} duration={1300} distance='500px' reset={true}>
+                    <Box mt='15px' bgColor='white' borderRadius='30px' mr={{ lg: '420px', base: '30px' }} ml={{ base: '30px', lg: '420px' }}>
+                        <Heading textAlign='center' size={{ base: 'md', lg: 'lg' }} fontWeight=''>Duration: 13 Weeks</Heading>
+                    </Box>
+                </RevealWrapper>
 
                 {/*Image */}
-                <Center pt='20px' borderRadius={{ lg: '30', base: '20px' }}>
-                    <Image style={imageStyles} src={CCNA} alt='CCNA' />
-                </Center>
+                <RevealWrapper origin='right' delay={300} duration={1400} distance='500px' reset={true}>
+                    <Center pt='20px' borderRadius={{ lg: '30', base: '20px' }}>
+                        <Image style={imageStyles} src={CCNA} alt='CCNA' />
+                    </Center>
+                </RevealWrapper>
 
                 {/*Course Description} */}
 
-                <Heading pt='20px' as='u' size={{ base: 'sm', lg: 'lg' }}>Course Description</Heading>
+                <RevealWrapper origin='right' delay={300} duration={1500} distance='500px' reset={true}>
+                    <Heading pt='20px' as='u' size={{ base: 'sm', lg: 'lg' }}>Course Description</Heading>
 
-                <Text pt='20px' textAlign='justify' fontSize={{ lg: '24px', base: '16px' }}>
-                    This course will focus on learning the basics of the networking and preparing for the new Cisco Certified Network Associate (CCNA) exam. This comprehensive course
-                    focuses on the solutions and technologies needed to implement and administer a broad range of modern networking and IT infrastructure
-                </Text>
+                    <Text pt='20px' textAlign='justify' fontSize={{ lg: '24px', base: '16px' }}>
+                        This course will focus on learning the basics of the networking and preparing for the new Cisco Certified Network Associate (CCNA) exam. This comprehensive course
+                        focuses on the solutions and technologies needed to implement and administer a broad range of modern networking and IT infrastructure
+                    </Text>
+                </RevealWrapper>
 
                 {/*1st Task*/}
 
-                <Heading pt='10px' size={{ base: 'xs', lg: 'md' }}>Textbook</Heading>
-                <Link href='https://www.amazon.com/CCNA-200-301-Official-Guide-Library/dp/1587147149/ref=sr_1_1' target="_blank" rel="noopener noreferrer">
-                    <Text as='u' fontSize={{ lg: '20px', base: '14px' }} color='blue'>
-                        https://www.amazon.com/CCNA-200-301-Official-Guide-Library/dp/1587147149/ref=sr_1_1
-                    </Text>
-                </Link>
+                <RevealWrapper origin='right' delay={300} duration={1600} distance='500px' reset={true}>
+                    <Heading pt='10px' size={{ base: 'xs', lg: 'md' }}>Textbook</Heading>
+                    <Link href='https://www.amazon.com/CCNA-200-301-Official-Guide-Library/dp/1587147149/ref=sr_1_1' target="_blank" rel="noopener noreferrer">
+                        <Text as='u' fontSize={{ lg: '20px', base: '14px' }} color='blue'>
+                            https://www.amazon.com/CCNA-200-301-Official-Guide-Library/dp/1587147149/ref=sr_1_1
+                        </Text>
+                    </Link>
+                </RevealWrapper>
             </Container>
         </Box >
     )
